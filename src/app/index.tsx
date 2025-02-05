69096ff8387447e8fefa439eaa0d2737f2150379
Gui Silva <adrianon10@gmail.com>
2025-02-05 03:43:25 -0300

    chore: add axios

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,13 +4,18 @@ import { Button } from '@/components/button'
 import { Steps } from '@/components/steps'
 import { Welcome } from '@/components/welcome'
 import { IconArrowRight } from '@tabler/icons-react-native'
+import { router } from 'expo-router'
 
 const Home = () => {
+  const handleGoToHomePage = () => {
+    router.navigate('/home')
+  }
+
   return (
     <View className="flex-1 gap-10 p-10">
       <Welcome />
       <Steps />
-      <Button>
+      <Button onPress={handleGoToHomePage}>
         <Button.Title>Começar</Button.Title>
         <Button.Icon icon={IconArrowRight} />
       </Button>
